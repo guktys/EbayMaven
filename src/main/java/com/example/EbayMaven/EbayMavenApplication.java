cbb7f221c66d403aaec734d9ed817c24e3958fc3
genia <guk.genia@gmail.com>
2023-05-23 00:28:05 +0300

    first

---
+++ src/main/java/com/example/EbayMaven/EbayMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.EbayMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayMavenApplication.class, args);
+	}
+
+}
